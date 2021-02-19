b006b78c39d5306e300b0962790b46e99c3aaab5
LeeDongHo <edh1021@gmail.com>
2021-02-19 23:46:11 +0900

    lombok hamcrest is slove

--- src/test/java/kr/domo/book/springboot/web/HelloControllerTest.java
+++ src/test/java/kr/domo/book/springboot/web/HelloControllerTest.java
@@ -9,9 +9,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 //lombok
-import org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -54,7 +53,7 @@ public class HelloControllerTest {
         .param("name",name)
         .param("amount",String.valueOf(amount)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount",is(amount)));
     }
 }
